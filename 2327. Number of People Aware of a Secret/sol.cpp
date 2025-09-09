dec734d41b61efa088d64ab3d1944157161157e9
Omar Romdhane <41232805+omaromdhane@users.noreply.github.com>
2025-09-09 21:57:53 +0100

    add 2327 cpp impl

---
+++ 2327. Number of People Aware of a Secret/sol.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        long long mod = 1000000007;
+        long long res = 0;
+        vector<long long> people(n,0);
+        people[0]=1;
+        for(int i=0;i<n;i++){
+            if(i>=n-forget){
+                res+=people[i]%mod;
+            }
+            if(i+delay<n){
+                for(int j=i+delay;j<min(i+forget,n);j++){
+                    people[j]+=people[i]%mod;
+                }
+            }
+            cout << res << " " << people[i] << " " << people[min(i+forget,n-1)] << endl;
+        }
+        return (int)(res % mod);
+    }
+};
